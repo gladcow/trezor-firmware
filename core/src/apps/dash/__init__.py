b8a240dd44f02f069674164a55173ac75bf7760f
gladcow <sergey@dash.org>
2019-08-14 15:09:46 +0300

    ProUpRegTx processing

--- core/src/apps/dash/__init__.py
+++ core/src/apps/dash/__init__.py
@@ -6,3 +6,4 @@ def boot():
     ns = [["secp256k1"]]
     wire.add(MessageType.DashSignProRegTx, __name__, "sign_dip2_tx", ns)
     wire.add(MessageType.DashSignProUpServTx, __name__, "sign_dip2_tx", ns)
+    wire.add(MessageType.DashSignProUpRegTx, __name__, "sign_dip2_tx", ns)
